d6c812cb11325998974cedb05e1429520c657c4e
amwankhe <amwankhe@cisco.com>
2025-08-16 09:30:29 +0530

    Update app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -16,28 +16,41 @@ Please enter the sensor and configuration data below to get a prediction.
 
 # User input
 age = st.number_input("age (max)", min_value=18, max_value=100, value=100)
-TypeofContact = st.selectbox("TypeofContact", ["Company Invited", "Self Inquiry"])
-CityTier = st.selectbox("CityTier", ["Tier 1", "Tier 2", "Tier 3"])
-Occupation = st.selectbox("Occupation", ["Salaried", "Self Employed", "Business Owner"])
-Gender = st.selectbox("Gender", ["Male", "Female"])
-NumberOfPersonVisiting = st.number_input("NumberOfPersonVisiting", min_value=1, max_value=10, value=1)
-PreferredPropertyStar = st.number_input("PreferredPropertyStar", min_value=1, max_value=5, value=1)
-MaritalStatus = st.selectbox("MaritalStatus", ["Single", "Married", "Divorced"])
-NumberOfTrips = st.number_input("NumberOfTrips", min_value=1, max_value=10, value=1)
-Passport = st.selectbox("Passport", ["Yes", "No"])
-OwnCar = st.selectbox("OwnCar", ["Yes", "No"])
-NumberOfChildrenVisiting = st.number_input("NumberOfChildrenVisiting", min_value=0, max_value=10, value=0)
-Designation = st.selectbox("Designation", ["Executive", "Managerial", "Professional", "Other"])
-MonthlyIncome = st.number_input("MonthlyIncome", min_value=0, max_value=100000, value=0)
+type_of_contact = st.selectbox("TypeofContact", ["Company Invited", "Self Inquiry"])
+city_tier = st.selectbox("CityTier", ["Tier 1", "Tier 2", "Tier 3"])
+cccupation = st.selectbox("Occupation", ["Salaried", "Self Employed", "Business Owner"])
+gender = st.selectbox("Gender", ["Male", "Female"])
+number_of_person_visiting = st.number_input("NumberOfPersonVisiting", min_value=1, max_value=10, value=1)
+preferre_Property_star = st.number_input("PreferredPropertyStar", min_value=1, max_value=5, value=1)
+marital_status = st.selectbox("MaritalStatus", ["Single", "Married", "Divorced"])
+number_of_trips = st.number_input("NumberOfTrips", min_value=1, max_value=10, value=1)
+passport = st.selectbox("Passport", ["Yes", "No"])
+own_car = st.selectbox("OwnCar", ["Yes", "No"])
+number_of_children_visiting = st.number_input("NumberOfChildrenVisiting", min_value=0, max_value=10, value=0)
+designation = st.selectbox("Designation", ["Executive", "Managerial", "Professional", "Other"])
+monthly_income = st.number_input("MonthlyIncome", min_value=0, max_value=100000, value=0)
 
 # Assemble input into DataFrame
 input_data = pd.DataFrame([{
-  'Age': age                                 
+  'Age': age,
+  'TypeofContact': type_of_contact,
+  'CityTier': city_tier,
+  'Occupation': cccupation,
+  'Gender': gender,
+  'NumberOfPersonVisiting': number_of_person_visiting,
+  'PreferredPropertyStar': preferre_Property_star,
+  'MaritalStatus': marital_status,
+  'NumberOfTrips': number_of_trips,
+  'Passport': passport,
+  'OwnCar': own_car,
+  'NumberOfChildrenVisiting': number_of_children_visiting,
+  'Designation': designation,
+  'MonthlyIncome': monthly_income                                 
 }])
 
 
-if st.button("Predict Tourim Package"):
+if st.button("Predict Wellness Tourim Package"):
     prediction = model.predict(input_data)[0]
-    result = "Machine Failure" if prediction == 1 else "No Failure"
+    result = "Wellness Tourim Package accepted" if prediction == 1 else "not Accepted"
     st.subheader("Prediction Result:")
     st.success(f"The model predicts: **{result}**")
